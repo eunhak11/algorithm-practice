269d63c7953250e1d53b84f2a580791ac1786300
eunhak <brianlee1111@naver.com>
2025-07-25 13:59:41 +0900

    2025.07.25 [Level2/Python] 캐시

--- 프로그래머스/Level2/2018KakaoBlined_[1차]캐시.py
+++ 프로그래머스/Level2/2018KakaoBlined_[1차]캐시.py
@@ -1,7 +1,8 @@
 """
 2018 KAKAO BLIND RECRUITMENT
 [1차] 캐시
-https://school.programmers.co.kr/learn/courses/30/lessons/17680
+문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/17680
+블로그 링크 :
 """
 
 from collections import deque
@@ -9,25 +10,29 @@ from collections import deque
 def solution(cache_size, cities):
 
     answer = 0
-    dq_cache = deque()
+    dq_cache = deque() # deque를 사용한 문제 풀이
 
-    if cache_size == 0:
-        return len(cities) * 5
+    if cache_size == 0: # 캐시가 비어있는 경우 (miss)
+        return len(cities) * 5 #
 
     else:
+        # 대소문자를 구분하지 않기 때문에 모두 대문자로 변환해서 풀이 진행
         for city in cities:
             city = city.upper()
 
             # hit
             if city in dq_cache:
+                # LRU이기 때문에 deque에서 지우고 맨 오른쪽에 다시 넣을 것이다.
                 dq_cache.remove(city)
                 answer += 1
             # miss
             else:
+                # 캐시가 가득 차있는 경우는 LRU에 따라 가장 오랫동안 사용되지 않은(가장 왼쪽) 도시 삭제
                 if len(dq_cache) >= cache_size:
                     dq_cache.popleft()
                 answer+=5
 
+            # deque 가장 오른쪽에 방금의 도시를 추가한다.
             dq_cache.append(city)
 
     return answer
