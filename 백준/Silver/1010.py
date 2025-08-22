dc3d233739b8a4252497ab669ed4add139e4773b
eunhak <brianlee1111@naver.com>
2025-08-22 11:53:25 +0900

    2025.08.22 [백준 1010] 다리놓기

---
+++ 백준/Silver/1010.py
@@ -0,0 +1,13 @@
+"""
+Silver5
+다리 놓기
+문제 링크 : https://www.acmicpc.net/problem/1010
+블로그 링크 :
+"""
+
+import math
+
+t = int(input())
+for i in range(t):
+    n, m = map(int, input().split())
+    print(math.comb(m, n))
